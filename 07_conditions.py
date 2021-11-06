ee1a3fb3fd53906a9405a69b7a6b004ac8f817fa
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-06 17:00:22 +0100

    update

---
+++ 07_conditions.py
@@ -0,0 +1,27 @@
+number = 5
+
+#      True
+#if number != 10:
+    # this runs if True
+    # print("Number is not equal to 10")
+
+
+#     True     and    True
+if number >= 5 and number <= 10:
+    print("Number is between 5 and 10")
+
+
+#     False   or    True
+if number < 5 or number > 10:
+    print("Number is less than 5")
+    print("Number is greater than 10")
+
+
+if number == 5:
+    print("Number is 5")
+elif number == 8:
+    print("Number is 8")
+elif number == 10:
+    print("Number is 10")
+else:
+    print("I couldn't find my favorite numbers :(((")
