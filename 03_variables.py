a19be5bc3e95ef8a7f22a9931b9cad734efd345a
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-06 15:27:20 +0100

    update

---
+++ 03_variables.py
@@ -0,0 +1,17 @@
+# variable definition
+my_name = "Vári Róbert"
+
+
+# variable call
+print(my_name)
+
+my_age = 25
+print(my_age)
+
+# overrides my_age above
+my_age = 18
+print(my_age)
+
+
+# formatted string
+print(f"Hello. My name is {my_name}. I'm {my_age} years old.")
