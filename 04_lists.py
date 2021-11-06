4aa43b95bc0ffc3ca3348343752c3e480af2f456
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-06 15:37:50 +0100

    update

---
+++ 04_lists.py
@@ -0,0 +1,13 @@
+#           0          1         2         3
+names = ["Robert", "Kriszta", "Csaba", "Tamás"]
+
+#                0  1  2  3    4
+lucky_numbers = [4, 8, 2, 9, names]
+
+print(names[-1])
+print( len(names) )
+
+print(lucky_numbers[4][1])
+
+# index error
+print(lucky_numbers[22])
