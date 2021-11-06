82af39eb121046bd63723811d1714bd29a47cbfb
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-06 17:30:07 +0100

    update

--- 08_for_loop.py
+++ 08_for_loop.py
@@ -1,4 +1,25 @@
-names = ["robert", "csaba", "kriszta"]
+names = ["robert", "csaba", "kriszta", "tamás", "csilla"]
+
+# sima for loop
+#for name in names:
+    #print(name)
+
+# skip item in a list
+for name in names:
+    if name == "kriszta":
+        continue
+    print(name)
+
 
 for name in names:
+    if name == "kriszta":
+        break
     print(name)
+
+for name in names:
+    if names.index(name) == 3:
+        break
+
+
+for index, name in enumerate(names):
+    print(index, name)
