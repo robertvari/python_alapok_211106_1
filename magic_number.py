28469a4f27b2c9444738dd138158454a6a99073c
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-06 17:53:47 +0100

    update

---
+++ magic_number.py
@@ -0,0 +1,22 @@
+min_number = 1
+max_number = 10
+max_tries = 3
+
+magic_number = 6
+
+print("="*50, "MAGIC NUMBER GAME", "="*50)
+print(f"I'm thinking of a number between {min_number} and {max_number}.\nCan you guess my number?")
+print("="*120)
+
+user_response = input("Your guess:")
+
+while user_response != magic_number:
+    max_tries -= 1
+
+    print(f"Wrong answer. You have {max_tries} tries left.")
+    user_response = input("Your guess:")
+
+if user_response == magic_number:
+    print(f"You win! My number was: {magic_number}")
+else:
+    print("Game over... maybe next time!")
