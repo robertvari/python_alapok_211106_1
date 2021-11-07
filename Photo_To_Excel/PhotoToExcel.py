d0bcf01ffbc3fcf7b5cf4202ddd8215a5519abe9
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-07 16:57:39 +0100

    update

--- Photo_To_Excel/PhotoToExcel.py
+++ Photo_To_Excel/PhotoToExcel.py
@@ -1,6 +1,6 @@
 import os
 from openpyxl import Workbook
-from PIL import Image
+from PIL import Image, ExifTags
 
 # open folder and get all files and folders
 photo_folder = r"C:\Work\_PythonSuli\pycore-211106\photos"
@@ -22,3 +22,28 @@ for i in file_list:
 
 # list comprehension
 # clean_file_list = [i for i in file_list if ".jpg" in i.lower()]
+
+# create excel workbook
+workbook = Workbook()
+sheet = workbook.active
+
+sheet["A1"] = "File Path"
+sheet["B1"] = "Date"
+sheet["C1"] = "Size"
+sheet["D1"] = "Camera"
+sheet["E1"] = "Lens"
+sheet["F1"] = "ISO"
+
+# open photos and get exif data
+for index, photo_file in enumerate(clean_file_list):
+    img = Image.open(photo_file)
+    image_size = img.size
+
+    row = index + 3
+
+    sheet[f"A{row}"] = photo_file
+    sheet[f"C{row}"] = f"{image_size[0]}x{image_size[1]}"
+
+
+excel_file = os.path.join(photo_folder, "photo_data.xlsx")
+workbook.save(excel_file)
