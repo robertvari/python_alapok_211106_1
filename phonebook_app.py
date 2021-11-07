f7194cd8a59c80f9ac04f5bb6d22483a83f9174f
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-07 15:17:33 +0100

    update

--- phonebook_app.py
+++ phonebook_app.py
@@ -8,7 +8,17 @@ data_file = "phonebook.json"
 if os.path.exists(data_file):
     with open(data_file) as f:
         phonebook = json.load(f)
-        print(phonebook)
-# if not
-    # get data from user
-    # save data_file
+
+# get data from user
+name = input("Your name?")
+address = input("Your address?")
+email = input("Your email?")
+
+# add to phonebook dictionary
+phonebook["name"] = name
+phonebook["address"] = address
+phonebook["email"] = email
+
+# save data_file
+with open(data_file, "w") as f:
+    json.dump(phonebook, f)
