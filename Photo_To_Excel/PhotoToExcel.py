92d23e2d497dfcb261bb4cad83b8f328cd1d83ea
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-07 16:37:05 +0100

    update

--- Photo_To_Excel/PhotoToExcel.py
+++ Photo_To_Excel/PhotoToExcel.py
@@ -2,10 +2,12 @@ import os
 from openpyxl import Workbook
 from PIL import Image
 
-
+# open folder and get all files and folders
 photo_folder = r"C:\Work\_PythonSuli\pycore-211106\photos"
 file_list = os.listdir(photo_folder)
 
+
+# filter file_list to get only .jpg and .jpeg formats
 clean_file_list = []
 extensions = [".jpeg", ".jpg"]
 
@@ -17,7 +19,6 @@ for i in file_list:
 
     clean_file_list.append(os.path.join(photo_folder, i))
 
+
 # list comprehension
 # clean_file_list = [i for i in file_list if ".jpg" in i.lower()]
-
-print(clean_file_list)
