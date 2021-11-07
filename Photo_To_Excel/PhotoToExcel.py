cbfb8d35501a2e00db1e43dfc3e834f8d7ececd5
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-07 17:19:12 +0100

    update

--- Photo_To_Excel/PhotoToExcel.py
+++ Photo_To_Excel/PhotoToExcel.py
@@ -31,7 +31,7 @@ sheet["A1"] = "File Path"
 sheet["B1"] = "Date"
 sheet["C1"] = "Size"
 sheet["D1"] = "Camera"
-sheet["E1"] = "Lens"
+sheet["E1"] = "Focal Length"
 sheet["F1"] = "ISO"
 
 # open photos and get exif data
@@ -41,8 +41,31 @@ for index, photo_file in enumerate(clean_file_list):
 
     row = index + 3
 
+    # photo path
     sheet[f"A{row}"] = photo_file
+
+    # image size
     sheet[f"C{row}"] = f"{image_size[0]}x{image_size[1]}"
+
+    # get exif data
+    exif_data = img._getexif()
+    if not exif_data:
+        continue
+
+    for key, value in exif_data.items():
+        tag_name = ExifTags.TAGS.get(key)
+
+        if tag_name == "ISOSpeedRatings":
+            sheet[f"F{row}"] = value
+
+        if tag_name == "DateTime":
+            sheet[f"B{row}"] = value
+
+        if tag_name == "Model":
+            sheet[f"D{row}"] = value
+
+        if tag_name == "FocalLength":
+            sheet[f"E{row}"] = str(value)
 
 
 excel_file = os.path.join(photo_folder, "photo_data.xlsx")
